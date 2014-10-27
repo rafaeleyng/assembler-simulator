395b2b0bb52d8a65750a7c1badce37c0002e0bfd
Rafael Eyng <rafaeleyng@gmail.com>
2014-10-26 23:19:10 -0200

    basic functionality done

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+var opcodes = {
+  // R
+  add:  '100000',
+  sub:  '100010',
+  and:  '100100',
+  or:   '100101',
+
+  // I
+  addi: '001000',
+  andi: '001100',
+  ori:  '001101',
+
+  // J
+  j:    '000010',
+  jr:   '001000',
+};
+
+var componentsBits = {
+  rs: 5,
+  rt: 5,
+  rd: 5,
+  shamt: 5,
+  funct: 6,
+  imm: 16,
+  addr: 26,
+};
+
 var Format = function(format, instructions, components) {
   this.format = format;
   this.instructions = instructions;
@@ -8,7 +35,25 @@ var Format = function(format, instructions, components) {
     componentsValues[component] = ko.observable();
   }
   this.componentsValues = componentsValues;
-}
+};
+
+String.prototype.padleft = function(len, chr) {
+  var repeats = len - this.length;
+  var pad = '';
+  for (var i = 0; i < repeats; i++) {
+    pad += chr;
+  }
+  return pad + this;
+};
+
+function getBinary(int, bits) {
+  if (int>=0) {
+    return int.toString(2).padleft(bits, "0");
+  }
+  return (-int-1).toString(2)
+    .replace(/[01]/g, function(d){return +!+d;}) // hehe: inverts each char
+    .padleft(bits, "1");
+};
 
 var ViewModel = function() {
 
@@ -60,8 +105,11 @@ var ViewModel = function() {
     var components = this.format().components;
     for (var i in components) {
       var component = components[i];
-      // result += this.format().componentsValues[component]() + ' ';
-      result += parseInt(this.format().componentsValues[component]()).toString(2) + ' ';
+      if (component === 'op') {
+        result += opcodes[this.format().componentsValues[component]()] + ' ';
+      } else {
+        result += getBinary(parseInt(this.format().componentsValues[component]()), componentsBits[component]) + ' ';
+      }
     }
     this.results.push(result);
   };
